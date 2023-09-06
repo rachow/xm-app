f59888b564dd6d886253c7b61c4bf723e4a955b8
Rahel Chowdhury <rahelahmed79@gmail.com>
2023-09-07 00:00:25 +0100

    adding more relevant changes for app js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,28 @@
 require('./bootstrap');
+
+window.baseURL = process.env.MIX_APP_URL;
+
+/**
+ * icon set for fancy console.
+*/
+window.mad = String.fromCodePoint(0x1F621);
+window.scream = String.fromCodePoint(0x1F631);
+window.wave = String.fromCodePoint(0x1F44B);
+window.bug = String.fromCodePoint(0x1F41E);
+window.flame = String.fromCodePoint(0x1F525);
+window.wink = String.fromCodePoint(0x1F609);
+window.robot = String.fromCodePoint(0x1F916);
+window.points = String.fromCodePoint(0x1F4AF);
+window.spark = String.fromCodePoint(0x1F4A5);
+window.dizzy = String.fromCodePoint(0x1F4AB);
+window.bomb = String.fromCodePoint(0x1F4A3);
+window.rocket = String.fromCodePoint(0x1F680);
+
+/**
+ * restrict embedding to iframe
+ * @2023
+*/
+if(window.top != window.self){
+	top.location.href = document.location.href;
+}
+
