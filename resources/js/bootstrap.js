8eeebe6d164aef7c535fac957d9bcf6992ece72d
Rahel Chowdhury <rahelahmed79@gmail.com>
2023-09-06 23:59:59 +0100

    making the bootstrap file more lean

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,8 +4,6 @@ try {
     require('bootstrap');
 } catch (e) {}
 
-window.baseURL = process.env.MIX_APP_URL;
-
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
@@ -14,37 +12,54 @@ window.baseURL = process.env.MIX_APP_URL;
 
 window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
+window.axios.defaults.headers.common['Accept'] = 'application/json';
+window.axios.defaults.baseURL = '/ajax';
 
 /**
- * Icon set for fancy console logging.
+ * solving many things here.
 */
+window.axios.interceptors.response.use((response) => {
+	return response;
+}, (error) => {
+	if (error.response) {
+		let http_code = error.response.status;
+		if( http_code === 401) {
+			let err = 'Your session has expired.';
+			let paths = window.location.pathname.split("/");
+	
+			if (paths[1] != "login") {
+				console.log('error:'+err);
+				if($('#modal-info-alert').length){
+					$('.modal-info-text').html(err + '<br> You must login.');
+					$('#modal-info-alert').modal('show');
+					$('#modal-info-alert').find('button').bind("click", function(e){
+						
+						//window.location.href = '/login';	
+						window.location.reload(); /* reload will maintain the redirect back */
+					});
+				} else {
+					alert(err);
+					window.location.reload();
+				}
+			}
 
-window.mad = String.fromCodePoint(0x1F621);
-window.scream = String.fromCodePoint(0x1F631);
-window.wave = String.fromCodePoint(0x1F44B);
-window.bug = String.fromCodePoint(0x1F41E);
-window.flame = String.fromCodePoint(0x1F525);
-window.wink = String.fromCodePoint(0x1F609);
-window.robot = String.fromCodePoint(0x1F916);
-window.points = String.fromCodePoint(0x1F4AF);
-window.spark = String.fromCodePoint(0x1F4A5);
-window.dizzy = String.fromCodePoint(0x1F4AB);
-window.bomb = String.fromCodePoint(0x1F4A3);
-window.rocket = String.fromCodePoint(0x1F680);
+		} else if (http_code == 500) {
+			let err = 'We apologies for the error.';
+			console.log('error: '+err);
+		
+			// TODO: add to Bugsnag so engineers are aware.
+			let err_body = error.response.data;
+			//Bugsnag.notify(error);
 
-
-// load bug notifier
-Bugsnag.start({
-	apiKey: process.env.MIX_BUGSNAG_API_KEY,
-	appVersion: process.env.MIX_APP_VERSION //  app release version
+			if ($('#modal-error-alert').length) {
+				$('.modal-error-text').html(err + '<br>Our engineers are taking a look.');
+				$('#modal-error-alert').modal('show');
+			} else {
+				alert(err + "\r\nOur engineers are taking a look.");
+			}
+		}
+	}
+	return Promise.reject(error);
 });
 
-/**
- * restrict iframe - other options?
- * @2023
-*/
-if(window.top != window.self){
-	top.location.href = document.location.href;
-}
 
