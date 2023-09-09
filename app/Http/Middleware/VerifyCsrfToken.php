c40ae2d591664e064ab3bb8f4953c2ad519125dd
Rahel Chowdhury <rahelahmed79@gmail.com>
2023-09-10 00:09:54 +0100

    Adding logics to prevent the 419 token expired error for some occassions

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Illuminate\Support\Facades\Auth;
 
 class VerifyCsrfToken extends Middleware
 {
@@ -14,4 +17,24 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
+
+    /**
+     * $rachow - added as additional for checks and control
+     * of the except array.
+     *
+     * @param Illuminate\Http\Request
+     * @param \Closure
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // is the route logout
+        if ($request->route()->named('logout')) {
+            // verify user is guest, did the user get logged back in via 'RememberMe'
+            // cookie
+            if (!Auth::check() || Auth::guard()->viaRemember()) {
+                $this->except[] = route('logout');
+            }
+        }
+        return parent::handle($request, $next);
+    }
 }
