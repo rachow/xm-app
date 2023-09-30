983929dbbd667887f8c9a8b18ff0ee53b221b794
Rahel Chowdhury <rahelahmed79@gmail.com>
2023-09-30 13:08:03 +0100

    adding the fetch for symbol data from yahoo finance api

--- app/Http/Controllers/Ajax/AjaxRoutesController.php
+++ app/Http/Controllers/Ajax/AjaxRoutesController.php
@@ -29,12 +29,94 @@ class AjaxRoutesController extends AjaxController
     {
         $inputs = $request->all();
         $validator = Validator::make([],[]);           
+    
         if (empty($inputs['symbol'])) {
             $validator->errors()->add('symbol', 'you must provide a valid symbol.');
             $errors = $validator->errors();
             return $this->errorResponse($errors);
         }
-    } 
+
+        if (empty($inputs['region'])) {
+            $inputs['region'] = 'US';
+        }
+
+        /**
+         * serious caching needed here!
+         *  - Network latency
+         *  - TCP Bottlenecks
+         *  - Offload OHLCV data fetching from intermediary proxy.
+         *      - Cache data to AWS RDS/RDBMS / NoSQL / Time-Series DB
+         *      - Schedule processes to load data into DB
+         *      - Track (TPS/QPS)
+         *  - Any Broker?
+        */
+
+        $yahoo_api_url = config('app.yahoo_api_url'); 
+        $yahoo_api_key = config('app.yahoo_api_key');
+
+        $yahoo_url_parts = parse_url($yahoo_api_url);
+
+        /*
+        $response = Http::withHeaders([
+            'X-RapidAPI-Key'  => $yahoo_api_key,
+            'X-RapidAPI-Host' => $yahoo_url_parts['host'],
+        ])->get($yahoo_api_url, [
+            'symbol' => $inputs['symbol'],
+            'region' => $inputs['region'],
+        ]);
+         */
+        // lets skip the SSL certificate checks
+        $response = Http::withoutVerifying()->withHeaders([
+            'X-RapidAPI-Key'  => $yahoo_api_key,
+            'X-RapidAPI-Host' => $yahoo_url_parts['host'],
+        ])->get($yahoo_api_url, [
+            'symbol' => $inputs['symbol'],
+            'region' => $inputs['region'],
+        ]);
+
+        // Any transformer ?
+        // https://fractal.thephpleague.com/transformers/
+
+        $json = $response->json();
+        return $this->successResponse($json);
+    }
+
+    private function getSymbolValidation(Request $request)
+    {
+        // we could create a Form Request to sparate the logics
+        // for validation.
+
+        $attribute_msgs = [
+            'symbol.min'    => '3 letter symbol is required',
+            'symbol.max'    => '3 letter symbol is required',
+            'email.dns'     => ':attribute does not exist, according to MX records',
+            'email.rfc'     => ':attribute does not exist or is invalid',
+        ];
+
+        // see - valcanbuild.tech/laravel-dns-email-validation
+        // use $faker->freeEmail to get green unit tests for MX record checks!
+
+        $validator = Validator::make($request->all(),[
+            'symbol' => 'required|min:3|max:3',
+            'startdate' => ['required', function($attribute, &$value, $fail){
+                    if (preg_match("/(\d){2}\/(\d){2}\/(\d){4}/", $value)) {
+                        $date = str_replace('/', '-', $value);
+                        $date_parts = explode('-', $date);
+                        $value = $date_parts[2] . '-' . $date_parts[1] . '-' . $date_parts[0]; 
+                    } elseif (preg_match("/(\d){4}\/(\d){2}\/(\d){2}/", $value)) {
+                        $date = str_replace('/', '-', $value);
+                        $value = $date; 
+                    } else {
+                        $fail($attribute . ' is invalid format yyyy/mm/dd');
+                    }
+            }],
+            'enddate' => [],
+            'email' => 'required'
+        ]);
+
+        return $validator;
+    }
+
 
     /**
      * Grab all the available symbols.
